a22fcfb1fd0aab0278905763caa5a706107f80a9
Patrick Huffer <huffer@rhywun.com>
2020-03-31 23:46:45 -0400

    Cleanup

--- Cedict/ClientApp/src/app/app.module.ts
+++ Cedict/ClientApp/src/app/app.module.ts
@@ -6,10 +6,6 @@ import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { CedictComponent } from './cedict/cedict.component';
 import { AngularMaterialModule } from "./angular-material.module";
 import {MatButtonModule} from "@angular/material/button";
@@ -17,10 +13,6 @@ import {MatButtonModule} from "@angular/material/button";
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     CedictComponent
   ],
     imports: [
@@ -28,10 +20,7 @@ import {MatButtonModule} from "@angular/material/button";
         HttpClientModule,
         FormsModule,
         RouterModule.forRoot([
-            {path: '', component: HomeComponent, pathMatch: 'full'},
-            {path: 'counter', component: CounterComponent},
-            {path: 'fetch-data', component: FetchDataComponent},
-            {path: 'cedict', component: CedictComponent}
+            {path: '', component: CedictComponent}
         ]),
         BrowserAnimationsModule,
         ReactiveFormsModule,
