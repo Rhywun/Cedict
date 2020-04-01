795a8ecbd2982f1cbeff6df8e9805c982c0b6a83
Patrick Huffer <huffer@rhywun.com>
2020-04-01 13:10:19 -0400

    Cleanup and added LICENSE

--- Cedict/ClientApp/src/app/app.module.ts
+++ Cedict/ClientApp/src/app/app.module.ts
@@ -1,33 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { CedictComponent } from './cedict/cedict.component';
-import { AngularMaterialModule } from "./angular-material.module";
-import {MatButtonModule} from "@angular/material/button";
+import {AngularMaterialModule} from "./angular-material.module";
+import {AppComponent} from './app.component';
+import {CedictComponent} from './cedict/cedict.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CedictComponent
   ],
-    imports: [
-        BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
-        HttpClientModule,
-        FormsModule,
-        RouterModule.forRoot([
-            {path: '', component: CedictComponent}
-        ]),
-        BrowserAnimationsModule,
-        ReactiveFormsModule,
-        AngularMaterialModule,
-        MatButtonModule
-    ],
+  imports: [
+    BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: '', component: CedictComponent}
+    ]),
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    AngularMaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
